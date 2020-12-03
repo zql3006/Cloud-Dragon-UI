bc591c8278c6b8e9266d021e33445927cc9e0194
zouql <zql2828288@163.com>
2020-12-03 19:29:28 +0800

    添加button，配置parcel

---
+++ src/button.vue
@@ -0,0 +1,27 @@
+<template>
+  <button class="c-button">按钮</button>
+</template>
+<script>
+export default {
+  
+}
+</script>
+<style lang="scss">
+  .c-button{
+    font-size: var(--font-size);
+    height: var(--button-height);
+    border-radius: var(--border-radius);
+    border:1px solid var(--border-color);
+    background: var(--button-bg);
+    padding: 0 1em;
+    &:hover{
+      border-color: var(--border-color-hover);
+    }
+    &:active{
+      background-color: var(--button-active-bg);
+    }
+    &:focus{
+      outline: none;
+    }
+  }
+</style>
